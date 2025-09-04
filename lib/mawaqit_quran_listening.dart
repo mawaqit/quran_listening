ab849b52a89eda345a855e77e74fd8533aedb22a
SameeHsn <30621868+SameeHsn@users.noreply.github.com>
2025-09-04 13:53:40 +0500

    sample_dialouge

--- lib/mawaqit_quran_listening.dart
+++ lib/mawaqit_quran_listening.dart
@@ -1,7 +1,36 @@
+library;
+
+import 'package:flutter/material.dart';
+
+class CustomAlertBox {
+  static Future showCustomAlertBox({required BuildContext context, required Widget willDisplayWidget}) {
+    return showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(15))),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              willDisplayWidget,
+              MaterialButton(
+                color: Colors.white30,
+                child: Text('close alert'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          ),
+          elevation: 10,
+        );
+      },
+    );
+  }
+}
+
 /// A Calculator.
 class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
 }
-
-
